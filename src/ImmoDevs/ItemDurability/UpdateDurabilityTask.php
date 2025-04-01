da298cd6cd3093aa3db63eab1f5c95cf968293f1
ImmoDevs <anggafiyan87@gmail.com>
2025-04-02 03:13:06 +0700

    Release

--- src/ImmoDevs/ItemDurability/UpdateDurabilityTask.php
+++ src/ImmoDevs/ItemDurability/UpdateDurabilityTask.php
@@ -1,39 +1,10 @@
 <?php
 
-/**
- * MIT License
- *
- * Copyright (c) 2025 ImmoDevs
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- * Copyright is perpetual and does not expire.
- *
- * @auto-license
- */
-
 declare(strict_types=1);
 
 namespace ImmoDevs\ItemDurability;
 
 use pocketmine\player\Player;
-use pocketmine\item\Item;
 use pocketmine\scheduler\Task;
 
 class UpdateDurabilityTask extends Task
@@ -42,11 +13,11 @@ class UpdateDurabilityTask extends Task
     private Player $player;
     private int $slot;
 
-    public function __construct(ItemDurability $plugin, Player $player, Item $item)
+    public function __construct(ItemDurability $plugin, Player $player, int $slot)
     {
         $this->plugin = $plugin;
         $this->player = $player;
-        $this->slot = $player->getInventory()->getHeldItemIndex();
+        $this->slot = $slot;
     }
 
     public function onRun(): void
